8061c8b1a90e82acb56ee5830bf1575a2ce8c0f0
sonianb <sonia.botsarova@gmail.com>
2022-01-24 14:38:39 +0000

    create toggleMenu() function

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ const menuItems = document.querySelectorAll(".menuItem");
 const hamburger= document.querySelector(".hamburger");
 const closeIcon= document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+      menu.classList.remove("showMenu");
+      closeIcon.style.display = "none";
+      menuIcon.style.display = "block";
+    } else {
+      menu.classList.add("showMenu");
+      closeIcon.style.display = "block";
+      menuIcon.style.display = "none";
+    }
+  }
+  // Clicking on the hamburger button will call toggleMenu()
+  hamburger.addEventListener("click", toggleMenu);
