8ae8b1d70c8b967409aa488f997207c7849d8584
PJSalter <psalter88@googlemail.com>
2022-01-26 13:54:44 +0000

    js code that makes the slides move functionally

--- app.js
+++ app.js
@@ -25,4 +25,66 @@ function(menuItem) {
     menuItem.addEventListener("click", toggleMenu);
     }
 )
+
+//-------------- Carousel code ----------//
+
+let appreciation = document.getElementById("testim"),
+recomDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+recomInfo = Array.prototype.slice.call(document.getElementById("testim-content").children),
+leftArrowRecom = document.getElementById("left-arrow"),
+rightArrowRecom = document.getElementById("right-arrow"),
+recomSpeed = 4500,
+presentSlide = 0,
+presentActive = 0,
+recomTimer
+;
+
+window.onload = function () {
+  // The code for the testimonials
+  function startSlide(slide) {
+    for(let i = 0; i < recomDots.length; i++){
+      recomInfo[i].classList.remove("active");
+      recomInfo[i].classList.remove("inactive");
+      recomDots[i].classList.remove("active");
+    }
+    if(slide < 0){
+      slide = presentSlide = recomInfo.length - 1;
+    }
+    if(slide > recomInfo.length - 1){
+       slide = presentSlide = 0;
+    }
+    if(presentActive != presentSlide){
+      recomInfo[presentActive].classList.add("inactive");
+    }
+    recomInfo[slide].classList.add("active");
+    recomDots[slide].classList.add("active");
+
+    presentActive = presentSlide;
+
+    clearTimeout(recomTimer);
+    recomTimer = setTimeout(function () {
+      startSlide(presentSlide += 1);
+    }, recomSpeed)
+  }
+  
+   leftArrowRecom.addEventListener("click", function () {
+      startSlide(presentSlide -= 1);
+   })
+   rightArrowRecom.addEventListener("click", function () {
+    startSlide(presentSlide += 1);
+    })
+    
+    for(let j = 0; j < recomDots.length; j++){
+        recomDots[j].addEventListener("click", function() {
+          startSlide(presentSlide = recomDots.indexOf(this));
+        })
+    }
+    startSlide(presentSlide);
+}
+
+
+
+
+
+
   
